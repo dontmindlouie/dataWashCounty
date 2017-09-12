e3ea6eb2a554fa68fcbc693e652941ae07dca428
Jonathan <Jonathan@DESKTOP-5LVATAU>
2017-09-12 16:10:37 -0700

    searchGroup List, extract TaxLotID List from html

---
+++ dataWashCounty/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace dataWashCounty
+{
+    class ExtractTaxlot
+    {
+        public List<List<string>> InitSearchGroup()
+        {
+            List<List<string>> searchGroup = new List<List<string>>
+            {
+                new List<string> {"1","2","3" },
+                new List<string> { "N", "S" },
+                new List<string> { "0", "1", "2", "3", "4", "5", "6" },
+                new List<string> { "0", "1", "2", "3" },
+                new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+                new List<string> { "0", "A", "B", "C", "D" },
+                new List<string> { "0", "A", "B", "C", "D" },
+                new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+                new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+            };
+            return searchGroup;
+        }
+        public IEnumerable<string> SearchID(string searchTerm)
+        {
+            string rawHtmlIDList = findTaxLotID(searchTerm).Result;
+            //Console.WriteLine(rawHtmlIDList);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(rawHtmlIDList);
+            HtmlNodeCollection taxLotIDNode = htmlDoc.DocumentNode.SelectNodes("/html/body/table//tr/td/table//tr//td//a");
+            var IDNumber = taxLotIDNode.Select(node => node.InnerText);
+            return IDNumber;
+        }
+        public static async Task<string> findTaxLotID(string searchTerm)
+        {
+            var responseString = "";
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    {"theTLNO", searchTerm},
+                    {"theAddress", ""},
+                    {"theRnum", ""},
+                    {"OwnerName", ""},
+                    {"btnSubmit", "Search"}
+                };
+                client.BaseAddress = new Uri(@"http://washims.co.washington.or.us");
+                var content = new FormUrlEncodedContent(values);
+                //HttpRequestHeaders atPostHeader = client.DefaultRequestHeaders; // what does this even do?
+                var response = await client.PostAsync(@"/GIS/index.cfm?id=20&sid=2", content);
+                responseString = await response.Content.ReadAsStringAsync();
+            }
+            return responseString;
+        }
+
+    }
+}
--- dataWashCounty/Program.cs
+++ dataWashCounty/Program.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HtmlAgilityPack;
+
+//  Find all TaxLotIDs
+//    HtmlClient to extract raw html of taxLotID list
+//    HtmlAgilityPack to parse raw html into taxLotIDs
+//  Extract Taxlot data
+//    HtmlClient to extract raw html of each property data
+//    HtmlAgilityPack to parse raw html into lists
+//  Save all data to database
 
 namespace dataWashCounty
 {
@@ -10,7 +21,33 @@ namespace dataWashCounty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("first commit");
+            Console.WriteLine("\nPress enter to cycle through taxLotIDs");
+            Console.ReadLine();
+
+            ExtractTaxlot p = new ExtractTaxlot();
+            var searchGroup2 = p.InitSearchGroup();
+
+            for(int i = 0; i < searchGroup2.Count(); i++)
+            {
+                for (int j = 0; j < searchGroup2[i].Count; j++)
+                {
+                    Console.WriteLine(searchGroup2[i][j]);
+                }
+            }
+            Console.WriteLine("\nEnd of search term list.");
+            Console.WriteLine("\nPress Enter to extract taxlotID html");
+            Console.ReadLine();
+
+            string searchTerm1 = "1";
+            IEnumerable<string> taxLotIDList = p.SearchID(searchTerm1);
+            foreach(var entry1 in taxLotIDList)
+            {
+                Console.WriteLine(entry1);
+            }
+
+            Console.WriteLine("\nEnd. Press Enter to Exit.");
+            Console.ReadLine();
         }
     }
+
 }
